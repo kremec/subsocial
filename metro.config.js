835aeda0158c8167a830df47b741979f23492451
kremec <klemen.remec@gmail.com>
2026-09-13 10:55:06 +0200

    feat: add bundle analyzer and enable tree shaking

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,7 @@
+// Expo applies these optimizations only to production bundles.
+process.env.EXPO_UNSTABLE_METRO_OPTIMIZE_GRAPH = "1";
+process.env.EXPO_UNSTABLE_TREE_SHAKING = "1";
+
 const { getDefaultConfig } = require("expo/metro-config");
 
 /** @type {import("expo/metro-config").MetroConfig} */
